1b4788a53979cd6c85781a8be7605b711d05cf19
Surendra <surendrachokotiya@gmail.com>
2018-06-21 06:52:53 +0530

    add validation

---
+++ default.py
@@ -0,0 +1,4 @@
+spy_name = 'Bond'
+spy_salutation = 'Mr.'
+spy_age = 21
+spy_rating = 4.5
---
+++ test.py
@@ -0,0 +1,7 @@
+def add (a,b):
+       return a+b
+
+       a=10
+       b=20
+       c = add(a,b)
+       print(c)
